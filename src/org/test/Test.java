bedfe222e49d0796d138d047baadda7350389439
arunk0805 <revarun997@gmail.com>
2022-08-08 15:40:47 +0530

    test

---
+++ src/org/test/Test.java
@@ -0,0 +1,34 @@
+package org.test;
+
+public class Test {
+	
+	
+
+	private void empName() {
+		
+		System.out.println("Employee Name:Arunkumar");
+		
+
+	}
+	
+	private void empId() {
+		
+		System.out.println("Employee Id:777");
+		
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Test e = new Test();
+		
+		e.empName();
+		e.empId();
+		
+	}
+	
+
+}
+
+
+
